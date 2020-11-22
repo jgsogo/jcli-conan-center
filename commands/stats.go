5f88b72ffd90768983f69b4ad308b4770d4ad7b0
jgsogo <jgsogo@gmail.com>
2020-11-22 20:45:36 +0100

    show search results

--- commands/stats.go
+++ commands/stats.go
@@ -2,12 +2,34 @@ package commands
 
 import (
 	"errors"
-	"github.com/jfrog/jfrog-cli-core/plugins/components"
-	"github.com/jfrog/jfrog-client-go/utils/log"
+	"fmt"
+	"regexp"
 	"strconv"
+
 	"github.com/jfrog/jfrog-cli-core/artifactory/commands"
+	"github.com/jfrog/jfrog-cli-core/artifactory/commands/generic"
+	"github.com/jfrog/jfrog-cli-core/artifactory/spec"
 	"github.com/jfrog/jfrog-cli-core/artifactory/utils"
+	"github.com/jfrog/jfrog-cli-core/plugins/components"
+	"github.com/jfrog/jfrog-cli-core/utils/config"
+	"github.com/jfrog/jfrog-client-go/utils/log"
 )
+
+type Reference struct {
+	Name     string
+	Version  string
+	User     string
+	Channel  string
+	Revision string
+}
+
+func (ref *Reference) String() string {
+	if len(ref.User) > 0 {
+		return fmt.Sprintf("%s/%s@%s/%s#%s", ref.Name, ref.Version, ref.User, ref.Channel, ref.Revision)
+	} else {
+		return fmt.Sprintf("%s/%s#%s", ref.Name, ref.Version, ref.Revision)
+	}
+}
 
 func GetStatsCommand() components.Command {
 	return components.Command{
@@ -31,8 +53,8 @@ func getStatsFlags() []components.Flag {
 			DefaultValue: "",
 		},
 		components.StringFlag{
-			Name:        "ref-name",
-			Description: "Name of a reference. If not specified, it will iterate every reference",
+			Name:         "ref-name", // TODO: Implementation pending (this should act as a filter)
+			Description:  "Name of a reference. If not specified, it will iterate every reference",
 			DefaultValue: "",
 		},
 	}
@@ -41,29 +63,57 @@ func getStatsFlags() []components.Flag {
 func getStatsArguments() []components.Argument {
 	return []components.Argument{
 		{
-			Name:         "repo",
-			Description:  "Artifactory repository name",
+			Name:        "repo",
+			Description: "Artifactory repository name",
 		},
 	}
+}
+
+func searchReferences(repository string, rtDetails *config.ArtifactoryDetails) ([]Reference, error) {
+	// Search all the 'conanfile.py' files inside the repository
+	log.Output("Searching references in repository", repository)
+	references := []Reference{}
+
+	// Pattern to search
+	specFile := spec.NewBuilder().Pattern(repository + "/**/conanfile.py").IncludeDirs(false).BuildSpec()
+	validConanChars := "[a-zA-Z0-9_][a-zA-Z0-9_\\+\\.-]"
+	referencePattern := regexp.MustCompile(repository + "\\/(?P<user>" + validConanChars + "*)\\/(?P<name>" + validConanChars + "+)\\/(?P<version>" + validConanChars + "+)\\/(?P<channel>" + validConanChars + "*)\\/(?P<revision>[a-z0-9]+)\\/export\\/conanfile\\.py")
+	searchCmd := generic.NewSearchCommand()
+	searchCmd.SetRtDetails(rtDetails).SetSpec(specFile)
+	reader, err := searchCmd.Search()
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+	for searchResult := new(utils.SearchResult); reader.NextRecord(searchResult) == nil; searchResult = new(utils.SearchResult) {
+		m := referencePattern.FindStringSubmatch(searchResult.Path)
+		user := m[1]
+		if user == "_" {
+			user = ""
+		}
+		channel := m[4]
+		if channel == "_" {
+			channel = ""
+		}
+		references = append(references, Reference{Name: m[2], Version: m[3], User: user, Channel: channel, Revision: m[5]})
+	}
+	return references, nil
 }
 
 func statsCmd(c *components.Context) error {
 	if len(c.Arguments) != 1 {
 		return errors.New("Wrong number of arguments. Expected: 1, " + "Received: " + strconv.Itoa(len(c.Arguments)))
 	}
-	repository := c.Arguments[0]
-	log.Output("Inspect repository", repository)
-	
+
 	rtDetails, err := commands.GetConfig(c.GetStringFlagValue("server-id"), true)
 	if err != nil {
+		log.Error(err)
 		return err
 	}
-	/*
-	servicesManager, err := utils.CreateServiceManager(rtDetails, false)
-	if err != nil {
-		return err
-	}
-	*/
+
+	// Check if repository exists
+	repository := c.Arguments[0]
+	log.Output("Inspect repository", repository)
 	artAuth, err := rtDetails.CreateArtAuthConfig()
 	if err != nil {
 		log.Error("Error", err)
@@ -74,6 +124,25 @@ func statsCmd(c *components.Context) error {
 		log.Error("Error2", err)
 		return err
 	}
+
+	// Return references
+	references, err := searchReferences(repository, rtDetails)
+	if err != nil {
+		return err
+	}
+	log.Output("Found", len(references), "Conan references")
+	for _, ref := range references {
+		fmt.Println(ref.String())
+	}
+
+	/*
+		servicesManager, err := utils.CreateServiceManager(rtDetails, false)
+		if err != nil {
+			return err
+		}
+	*/
+
+
 
 	log.Output("Done!")
 	return nil
