7b0ff5d9bf78df11d9669b3a2b76430fe2a76d7b
jgsogo <jgsogo@gmail.com>
2020-12-08 19:54:08 +0100

    document types

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
+// Package main constains a plugin for JFrog CLI, the command line interface
+// to JFrog Artifactory. This plugin implements several commands realated to
+// Conan (https://conan.io) and ConanCenter (https://conan.io/center). Most
+// of these commands are intended to manage the Artifactory repository
+// where the packages in ConanCenter are stored.
 package main
 
 import (
 	"github.com/jfrog/jfrog-cli-core/plugins"
 	"github.com/jfrog/jfrog-cli-core/plugins/components"
 	"github.com/jgsogo/jcli-conan-center/commands"
-	//"github.com/jfrog/jfrog-cli-plugin-template/commands"
 )
 
 func main() {
